5ff4ed02480cd1ca7b97274208af4f4de9b640ac
Sacha Weatherstone <sachaw100@hotmail.com>
2024-10-06 22:11:45 +1000

    Enable publishing to Cargo & JSR

---
+++ packages/ts/mod.ts
@@ -0,0 +1,43 @@
+import * as Admin from "./lib/admin_pb.ts";
+import * as AppOnly from "./lib/apponly_pb.ts";
+import * as ATAK from "./lib/atak_pb.ts";
+import * as CannedMessages from "./lib/cannedmessages_pb.ts";
+import * as Channel from "./lib/channel_pb.ts";
+import * as ClientOnly from "./lib/clientonly_pb.ts";
+import * as Config from "./lib/config_pb.ts";
+import * as ConnectionStatus from "./lib/connection_status_pb.ts";
+import * as LocalOnly from "./lib/localonly_pb.ts";
+import * as Mesh from "./lib/mesh_pb.ts";
+import * as ModuleConfig from "./lib/module_config_pb.ts";
+import * as Mqtt from "./lib/mqtt_pb.ts";
+import * as PaxCount from "./lib/paxcount_pb.ts";
+import * as Portnums from "./lib/portnums_pb.ts";
+import * as PowerMon from "./lib/powermon_pb.ts";
+import * as RemoteHardware from "./lib/remote_hardware_pb.ts";
+import * as Rtttl from "./lib/rtttl_pb.ts";
+import * as StoreForward from "./lib/storeforward_pb.ts";
+import * as Telemetry from "./lib/telemetry_pb.ts";
+import * as Xmodem from "./lib/xmodem_pb.ts";
+
+export const Protobuf = {
+  Admin,
+  AppOnly,
+  ATAK,
+  CannedMessages,
+  Channel,
+  ClientOnly,
+  Config,
+  ConnectionStatus,
+  LocalOnly,
+  Mesh,
+  ModuleConfig,
+  Mqtt,
+  PaxCount,
+  Portnums,
+  PowerMon,
+  RemoteHardware,
+  Rtttl,
+  StoreForward,
+  Telemetry,
+  Xmodem,
+};
